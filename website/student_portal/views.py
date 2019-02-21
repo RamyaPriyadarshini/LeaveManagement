95618ee54f45fa0c5fabf1ee60abd27e61895b8f
unknown <ramyaphone78@gmail.com>
2019-02-21 14:46:56 +0530

    test

--- website/student_portal/views.py
+++ website/student_portal/views.py
@@ -31,21 +31,23 @@ class PersonalCreate(CreateView):
     model = Personal
     fields = ['name','dept','regno']
 
-def register(request):
-    form = UserForm(request.POST or None)
-    if form.is_valid():
-        user = form.save(commit=False)
-        username = form.cleaned_data['username']
-        password = form.cleaned_data['password']
-        user.set_password(password)
-        user.save()
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                albums = Personal.objects.filter(user=request.user)
-                return render(request, 'music/index.html', {'albums': albums})
-    context = {
-        "form": form,
-    }
-    return render(request, 'music/register.html', context)
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'student_portal/registration.html'
+    def get(self,request):
+        form = self.form_class(None)
+        return render(request,self.template_name,{'form':form})
+    def post(self,request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+            user = authenticate(username=username,password=password)
+            if user is not none:
+                if user.is_active:
+                    login(request.user)
+                    return redirect('portal:index')
+        return render(request,self.template_name,{'form':form}) 
